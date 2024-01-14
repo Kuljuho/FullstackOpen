c95c15c6702c9b8fd9f73b2f1c685709d2f6ac1f
Juho Kultala <128252330+Kuljuho@users.noreply.github.com>
2024-01-14 17:50:35 +0200

    Tehtävät 3, 4, 5 tehty. React Appia jatkettu.

--- 1. osa/1-2/src/App.jsx
+++ 1. osa/1-2/src/App.jsx
@@ -2,47 +2,51 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 }
 
-const Part = (props) => {
-  return <p>{props.part} {props.exercises}</p>
-}
-
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      {props.parts.map((part, index) => (
+        <p key={index}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
     </div>
-  )
+  );
 }
 
 const Total = (props) => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+
   return (
-    <p>Total number of exercises {props.total}</p>
+    <p>Total number of exercises {total}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3;
-
+  const course = { 
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+  ] 
+  }
   return (
   <div>
-    <Header course={course} />
-    <Content
-      part1={part1} exercises1={exercises1}
-      part2={part2} exercises2={exercises2}
-      part3={part3} exercises3={exercises3}
-      />
-    <Total total={total} />
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
-)
+  )
 }
 
 export default App
